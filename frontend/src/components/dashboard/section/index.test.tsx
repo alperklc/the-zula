30db84097a35e0dd6f16de6522e1830d528d7931
Alper Kılcı <alperkilci@gmail.com>
2024-09-21 18:05:26 +0200

    fix tests

--- frontend/src/components/dashboard/section/index.test.tsx
+++ frontend/src/components/dashboard/section/index.test.tsx
@@ -1,35 +1,34 @@
-import { describe, vi, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest'
+import { BrowserRouter } from 'react-router-dom'
 import { render } from '@testing-library/react'
 import { AllContentSection, DashboardSection } from './index'
-
-vi.mock('react-intl', () => ({
-  useIntl: () => ({ formatMessage: vi.fn() }),
-}))
 
 describe('DashboardSection', () => {
   it('WHEN an array of content is given THEN they are rendered correctly', () => {
     // arrange
     const rows = [
       {
-          typename: 'Note',
+          typename: 'NOTE',
           id: 'aabel2cGoibqDbkDIJf78',
           title: 'test note',
         },
         {  
-          typename: 'Bookmark',
+          typename: 'BOOKMARK',
           id: 'AJ0K4sAPIqrG84AvDLOSL',
           name: 'kek.jpg',
+          title: 'kek.jpg',
         },
         {
-          typename: 'Bookmark',
+          typename: 'BOOKMARK',
           id: '1FO5kHFBYZiFdxsdeblYu',
           title: 'Documentation - The Go Programming Language',
+          name: 'Documentation - The Go Programming Language',
       },
     ]
 
     // act
     const { queryByTestId, queryAllByTestId } = render(
-      <DashboardSection title='test' rows={rows} />,
+      <BrowserRouter><DashboardSection title='test' rows={rows} /></BrowserRouter>
     )
     const renderedContent = queryAllByTestId('content')
 
@@ -46,34 +45,30 @@ describe('AllContentSection', () => {
   it('WHEN all given numbers are 0 THEN the numbers are not displayed', () => {
     // act
     const { queryByTestId } = render(
-      <AllContentSection data={{ numberOfBookmarks: 0, numberOfNotes: 0 }} />,
+      <BrowserRouter><AllContentSection data={{ numberOfBookmarks: 0, numberOfNotes: 0 }} /></BrowserRouter>,
     )
-    const [numberOfNotes, numberOfFiles, numberOfBookmarks] = [
+    const [numberOfNotes, numberOfBookmarks] = [
       'numberOfNotes',
-      'numberOfFiles',
       'numberOfBookmarks',
     ].map((testId) => queryByTestId(testId))
 
     // assert
     expect(numberOfNotes?.textContent).toBe('')
-    expect(numberOfFiles?.textContent).toBe('')
     expect(numberOfBookmarks?.textContent).toBe('')
   })
 
   it('WHEN some of given numbers are NOT 0 THEN they are displayed', () => {
     // act
     const { queryByTestId } = render(
-      <AllContentSection data={{ numberOfBookmarks: 5, numberOfNotes: 4 }} />,
+      <BrowserRouter><AllContentSection data={{ numberOfBookmarks: 5, numberOfNotes: 4 }} /></BrowserRouter>,
     )
-    const [numberOfNotes, numberOfFiles, numberOfBookmarks] = [
+    const [numberOfNotes, numberOfBookmarks] = [
       'numberOfNotes',
-      'numberOfFiles',
       'numberOfBookmarks',
     ].map((testId) => queryByTestId(testId))
 
     // assert
     expect(numberOfNotes?.textContent).toBe(' (4)')
-    expect(numberOfFiles?.textContent).toBe('')
     expect(numberOfBookmarks?.textContent).toBe(' (5)')
   })
 })
--- frontend/src/utils/to-time-string.test.ts
+++ frontend/src/utils/to-time-string.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
 import { toTimeString } from './to-time-string'
 
 interface Test {
@@ -6,27 +6,28 @@ interface Test {
   expectedTimeSince: string
 }
 
-// const mockReturnValue = new Date('2021-05-13T12:24:39.649Z').getTime()
-
 describe('toTimeString', () => {
-  [
-    {
-      dateTime: '2021-05-10T13:11:18.139Z',
-      expectedTimeSince: '10.05',
-    },
-    {
-      dateTime: '2021-05-01T20:51:42.328Z',
-      expectedTimeSince: '01.05',
-    },
-    {
-      dateTime: '2021-05-13T13:20:00.328Z',
-      expectedTimeSince: '15:20',
-    },
-  ].map(({ dateTime, expectedTimeSince }: Test) => {
-    it('displays the given date correctly', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it.each`
+    dateTime | expectedTimeSince
+    ${'2024-05-10T13:11:18.139Z'} | ${'10.05.2024'}
+    ${'2024-05-01T20:51:42.328Z'} | ${'01.05.2024'}
+    ${'2024-05-13T13:20:00.328Z'} | ${'15:20'}
+  `(
+    'displays the given date correctly',
+    ({ dateTime, expectedTimeSince }: Test) => {
+      const date = new Date('2024-05-13T12:24:39.649Z')
+      vi.setSystemTime(date)
+
       const timeSince = toTimeString(dateTime)
 
       expect(timeSince).toEqual(expectedTimeSince)
     })
-  })
 })
